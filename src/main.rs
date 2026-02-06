80d191cdd3746f26ee4357d37c39eebd2797fc3a
Gautam011004 <divyagautam326@gmail.com>
2026-02-06 13:00:20 +0530

    set and get commands done

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
 #![allow(unused_imports)]
+use core::panic;
+use std::collections::btree_map::Values;
+
 use anyhow::{Error, Ok};
 use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}};
 
-use crate::resp::Value;
+use crate::{database::db, resp::Value};
 pub mod resp;
-pub mod db;
+pub mod database;
 
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let redisdb = db::new();
     loop {
         let (mut socket, _) = listener.accept().await.unwrap();
+        let redisdb = redisdb.clone();
         tokio::spawn(async move {
-            handle_connection(socket).await
+            handle_connection(socket, redisdb).await
         });
     }
 }
 
-async fn handle_connection(mut socket: TcpStream) {
+async fn handle_connection(mut socket: TcpStream, redisdb: db) {
     let mut handler = resp::RespHandler::new(socket);
 
     loop {
@@ -29,7 +34,14 @@ async fn handle_connection(mut socket: TcpStream) {
             match command.as_str() {
                 "ping" => Value::SimpleString("PONG".to_string()),
                 "echo" => args.first().unwrap().clone(),
-                "SET" => Value::SimpleString("OK".to_string()),
+                "SET" => {
+                    set_handle(&args, &redisdb).await.unwrap();
+                    Value::SimpleString("OK".to_string())
+                }
+                "GET" => {
+                    let value = get_handle(&args, &redisdb).await.unwrap();
+                    Value::SimpleString(value)
+                }
                 c => panic!("Cannot handle command {:?}",c)
             }
         }
@@ -48,7 +60,7 @@ fn extract_command(value: Value) -> Result<(String, Vec<Value>), Error>{
                 a.into_iter().skip(1).collect()
         ))
         },
-        _ => Err(anyhow::anyhow!("Unexpexted command format"))
+        _ => Err(anyhow::anyhow!("Unexpected command format"))
     }
 }
 fn unpack_bulk_str(value: Value) -> Result<String, Error> {
@@ -56,4 +68,27 @@ fn unpack_bulk_str(value: Value) -> Result<String, Error> {
         Value::BulkString(s) => Ok(s),
         _ => Err(anyhow::anyhow!("Unexpected command for a bulkstring"))
     }
+}
+async fn get_handle(args: &Vec<Value>, db: &db) -> Result<String, Error> {
+    let key_value = args[1].clone();
+    let key = match key_value {
+        Value::BulkString(s) => Some(s),
+        _ => panic!("Wrong arrguments for a get command")
+    }.unwrap();
+    let value = db.get(&key).await.unwrap();
+    Ok(value)
+}
+async fn set_handle(args: &Vec<Value>, db: &db) -> Result<(), Error> {
+    let key_value = args[1].clone();
+    let value_value = args[2].clone();
+    let key = match key_value {
+        Value::BulkString(s) => Some(s),
+        _ => panic!("Wrong arrguments for a set command")
+    }.unwrap();
+    let value = match value_value {
+        Value::BulkString(s) => Some(s),
+        _ => panic!("Wrong arrguments for a set command")
+    }.unwrap();
+    db.set(&key, &value).await.unwrap();
+    Ok(())
 }
